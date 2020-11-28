230133a668c8fed62c1d074edc2f2619652b48ed
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2020-11-28 12:14:49 +0300

    feat: добавлены переходы с главной

--- src/container/RootPage/RootPage.tsx
+++ src/container/RootPage/RootPage.tsx
@@ -6,22 +6,36 @@ import Icon16Poll from '@vkontakte/icons/dist/16/poll';
 import Icon16DonateOultine from '@vkontakte/icons/dist/16/donate_oultine';
 import logo from "../../assets/logo.png";
 import tournament from "../../assets/tournament_example.png";
+import { useRouter } from "@happysanta/router";
+import { PAGE_ACHIEVEMENTS, PAGE_ORGANIZE, PAGE_PROFILE, PAGE_RATING, PAGE_SEARCH } from "../../router/router";
 
 export const RootPage = ({id}: {id: string}) => {
+  const router = useRouter();
+
     return (
         <Panel id={id}>
             <Div>
               <img src={logo} alt={'Logo'} />
               <Text weight={"regular"}>Первое киберспортивное приложение Вконтакте!</Text>
               <Div>
-                <Button>Найти турнир</Button>
-                <Button>Организовать турнир</Button>
-                <Button>Профиль</Button>
+                <Button onClick={() => router.pushPage(PAGE_SEARCH)}>Найти турнир</Button>
+                <Button onClick={() => router.pushPage(PAGE_ORGANIZE)}>Организовать турнир</Button>
+                <Button onClick={() => router.pushPage(PAGE_PROFILE)}>Профиль</Button>
               </Div>
               <TournamentCard title={'Супер-турнир'} discipline={'Dota 2'} nextMatch={'Босс'} time={'18:00'} image={tournament} />
               <Div>
-                <Button before={<Icon16Poll />}>Рейтинг</Button>
-                <Button before={<Icon16DonateOultine />}>Достижения</Button>
+                <Button
+                  before={<Icon16Poll />}
+                  onClick={() => router.pushPage(PAGE_RATING)}
+                >
+                  Рейтинг
+                </Button>
+                <Button
+                  before={<Icon16DonateOultine />}
+                  onClick={() => router.pushPage(PAGE_ACHIEVEMENTS)}
+                >
+                  Достижения
+                </Button>
               </Div>
             </Div>
         </Panel>
