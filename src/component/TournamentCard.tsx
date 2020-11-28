e38192d1d9969317209eb15497e6e978ac5fb050
Grigorii Kostarev <grik2001@gmail.com>
2020-11-29 00:52:51 +0300

    error: problem with tranfering data to card

--- src/component/TournamentCard.tsx
+++ src/component/TournamentCard.tsx
@@ -8,7 +8,8 @@ export interface TournamentCardProps {
     place?: string;
     date: string;
     image: string;
-    type: 'current' | 'passed' | 'planned'
+    type: 'current' | 'passed' | 'planned';
+    registration?: 'registered' | 'planned';
 }
 
 export const TournamentCard = (props: TournamentCardProps) => {
@@ -18,13 +19,22 @@ export const TournamentCard = (props: TournamentCardProps) => {
             <TextContainer>
                 <StyledTitle weight={"regular"} level={"2"}>{props.title}</StyledTitle>
                 <StyledTitle weight={"regular"} level={"3"}>{props.discipline}</StyledTitle>
-                {props.type === 'current' ? (
-                    <>
-                        <StyledText weight={"regular"}>Следующий соперник:</StyledText>
-                        <StyledText weight={"regular"}>{props.nextMatch}, {props.date}</StyledText>
-                    </>
-                ) : null}
-                {['passed', 'planned'].includes(props.type) ? (<StyledText weight={"regular"}>{props.date}</StyledText>) : null}
+                {
+                    props.type === 'current' ? (
+                        <>
+                            <StyledText weight={"regular"}>Следующий соперник:</StyledText>
+                            <StyledText weight={"regular"}>{props.nextMatch}, {props.date}</StyledText>
+                        </>
+                    ) : null
+                }
+                {['passed', 'planned'].includes(props.type) ?
+                    (<>
+                        <StyledText weight={"regular"}>{props.date}</StyledText>
+                        <StyledText weight={"regular"}>{
+                            props.registration === 'planned' ? 'Заявлен' : 'Вы зарегистрированы'
+                        }</StyledText>
+                    </>)
+                    : null}
                 {props.type === 'passed' ? (<StyledText weight={"regular"}>{props.place}</StyledText>) : null}
             </TextContainer>
         </CardContainer>
