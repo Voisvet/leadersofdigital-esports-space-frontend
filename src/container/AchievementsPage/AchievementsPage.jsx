8b2a48649a9cc615b3cf929f830b7c8e95332aa8
Grigorii Kostarev <grik2001@gmail.com>
2020-11-28 12:34:10 +0300

    feat: add page with achievements

--- src/container/AchievementsPage/AchievementsPage.jsx
+++ src/container/AchievementsPage/AchievementsPage.jsx
@@ -1,10 +1,51 @@
 import React from "react";
-import {Panel} from "@vkontakte/vkui";
+import {Cell, Panel, PanelHeader, PanelHeaderBack, Avatar} from "@vkontakte/vkui";
 
 export const AchievementsPage = ({id}) => {
+    const achievements = [
+        {
+            name: 'Первый шаг',
+            description: 'Приянять участие в турнире'
+        },
+        {
+            name: 'Частый гость',
+            description: 'Принять участие в 10 турнирах'
+        },
+        {
+            name: 'Гуру соревнований',
+            description: 'Принять участие в 50 турнирах'
+        },
+        {
+            name: 'Начало положено',
+            description: 'Выиграть 1 турнир'
+        },
+        {
+            name: 'Я у мамы молодец',
+            description: 'Выиграть 5 турниров'
+        },
+        {
+            name: 'Давайте следующий',
+            description: 'Выиграть 10 турниров'
+        }
+        ];
+
     return (
         <Panel id={id}>
-            test
+            <PanelHeader
+                 left={<PanelHeaderBack onClick={() => {}}/>}
+            >
+                Достижения
+            </PanelHeader>
+            {
+                achievements.map((achievement) => {
+                    return <Cell
+                        description={achievement.description}
+                        before={<Avatar />}
+                    >
+                        {achievement.name}
+                    </Cell>;
+                })
+            }
         </Panel>
     );
 }
